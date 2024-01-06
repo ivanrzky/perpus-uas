93e6f5b9ef041172f3683c767e6f87d52533a969
Ivan <hello.vaann@gmail.com>
2024-01-06 23:31:58 +0700

    controller kategori

--- app/Http/Controllers/Petugas/KategoriController.php
+++ app/Http/Controllers/Petugas/KategoriController.php
@@ -13,6 +13,7 @@ class KategoriController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /** controller kategori */
     public function __invoke(Request $request)
     {
         return view('petugas/kategori/index');
